1af989e226203e5f854db99596fc100bed5e26c6
Matias Meneses <mat.meneses47@gmail.com>
2023-06-27 15:26:19 -0300

    first commit

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img\Logo Aplicación.png">
+    <link rel="stylesheet" href="css/styleHomepage.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+     integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+     integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+     integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"
+     integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+    
+    <title>Fast tracker</title>
+</head>
+<header>
+     <div class="headerDivPrincipal">
+
+          <div class="divsHijosHeader">
+            <a href="index.html"><img class="logoHeader" src="img\Logo Aplicación.png" alt="Fastracker">Inicio </a>
+          </div>
+           
+            <div class="divsHijosHeader" style="text-decoration: none;">
+              <button> 
+              <a href="">Presentacion</a>
+            </button>
+            </div>
+            <div class="divsHijosHeader">
+               <div class="quienesSomos">
+                <button>Quienes Somos
+                </button>
+                <div class="opcionesQuienesSomos"> 
+                  <a href="">Ubicaciones</a>
+                  <a href="">Trabaja con nosotros</a>
+                </div>
+               </div>
+            </div>
+            <div class="divsHijosHeader" style="text-decoration: none;">
+              <button> 
+              <a href="">Iniciar sesion</a>
+            </button>
+            </div>  
+    </div>
+</header>
+<body>
+  
+    <div class="divPrincipalBody">
+         <div class="contenido">hola soy un div en el body
+                <div>FastTracker ofrece un increible servicio de mensajeria y paqueteria con una red que abarca todo uruguay con sucursales añadidas con el tiempo</div>
+                                     
+                                          <div id="sliderConFotos" class="carousel slide" data-bs-ride="carousel">
+                                            <div id="carouselExampleIndicators" class="carousel slide">
+                                              <div class="carousel-indicators">
+                                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                              
+                                              </div>
+                                              <div class="carousel-inner">
+                                                <div class="carousel-item active c-item">
+                                                  <img class="d-block w-100 c-img" src="img/mercadolibre.jpg" alt="dac">
+                                                </div>
+                                                <div class="carousel-item c-item">
+                                                  <img class="d-block w-100 c-img" src="img/dac.png" alt="MercadoLibre">
+                                                </div>
+                                                
+                                              </div>
+                                              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                                <span class="visually-hidden">Previous</span>
+                                              </button>
+                                              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                                <span class="visually-hidden">Next</span>
+                                              </button>
+                                            </div>
+                                     
+                <span id="slide-1"></span>
+                 <span id="slide-2"></span>
+                   <br>
+                          <div class="paginasDisponibles">
+                              <div class="paginasDisponiblesHijos">
+                                  <button>
+                                     <a href="">Buscar paquete</a>
+                                  </button>
+                              </div>
+                              <div class="paginasDisponiblesHijos">
+                                  <button>
+                                   <a href="">Almacenes</a>
+                                  </button>
+                              </div>
+                              <div class="paginasDisponiblesHijos">
+                                  <button>
+                                   <a href="">Choferes</a>
+                                  </button>
+
+                              </div>
+                          </div>
+         </div>   
+        </div>
+         
+      
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+     integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+     integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"
+     integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+    
+</body>
+<footer>
+   <div class="divPrincipalFooter"> 
+        <div class="divHijoFooter">
+        hola soy un footer
+        </div>
+        <div class="divHijoFooter">
+          FastTracker todos los derechos reservados
+        </div>
+        <div class="divHijoFooter">
+          Redes sociales
+        </div>
+</div>
+</footer>
+</html>
