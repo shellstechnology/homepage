4dd057b494ad63dc790fe081cc5c50eed67c0b72
Ignacio Tejera <itejera01@gmail.com>
2023-09-07 17:23:29 -0300

    Agregado de footer, header en ingles. Correcciones en el homepage traducido

---
+++ resources/views/footerEn.blade.php
@@ -0,0 +1,14 @@
+<head>
+    <link rel="stylesheet" href="css\styleFooter.css">
+</head>
+
+<footer>
+    <div class="divPrincipalFooter">
+        <div class="divHijoFooter">
+            © FastTracker all right reserved
+        </div>
+        <div class="divHijoFooter">
+            Social Media
+        </div>
+    </div>
+</footer>
---
+++ resources/views/headerEn.blade.php
@@ -0,0 +1,37 @@
+<head>
+    <link rel="stylesheet" href="css\styleHeader.css">
+</head>
+
+<header>
+    <div class="headerDivPrincipal">
+
+        <div class="divsHijosHeader">
+            <a href="http://localhost:8000/homepageEn"><img class="logoHeader" src="img\Logo Aplicación.png" alt="Fastracker">
+            </a>
+        </div>
+        <div class="divsHijosHeader">
+            <div class="quienesSomos">
+                <button>Who are We
+                </button>
+                <div class="opcionesQuienesSomos">
+                    <a href="">Ubications</a>
+                    <a href="">Work with us</a>
+                    <a href="http://localhost:8000/presentacion">Presentation</a>
+                </div>
+            </div>
+        </div>
+        <div class="divsHijosHeader">
+            <button>
+                <a href="">Log In</a>
+            </button>
+        </div>
+        <div class="divsHijosHeader">
+            <a href="http://localhost:8000/homepage" target="_blank">
+                <div class="checkboxCambioIdioma">
+                    <input type='checkbox' />
+                    <span class="encender"></span>
+                </div>
+            </a>
+        </div>
+    </div>
+</header>
