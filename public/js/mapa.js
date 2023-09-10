fe0efd77be5199df0f0cfe4c37a7be15dba57cc7
AlejandroJMP24 <alejandromachado85@gmail.com>
2023-09-09 22:49:05 -0300

    Mapa modificado para mostrar almacenes

--- public/js/mapa.js
+++ public/js/mapa.js
@@ -1,15 +1,8 @@
-var puntos = [];
-var nombres = [];
-var ruta = [];
-var planRuta = [];
-var point;
-var lastpoint;
-var r = null;
+var puntos = ['-34.90569, -56.18801','-34.8083, -56.2179'];
 var ubicacion = null;
 var map;
 var circulo = null;
 var marcador = null;
-var controlBoton = 1;
 var markers = [];
 
 var ico1 = L.icon({
@@ -26,38 +19,27 @@ var ico2 = L.icon({
     popupAnchor: [-3, -76]
 })
 
-function agregarPunto() {
-    var nombreInput = document.getElementById("nombre");
-    var puntosInput = document.getElementById("puntos");
-    var coordenadas = puntosInput.value.split(",").map(function (coordenada) {
-        return parseFloat(coordenada.trim());
-    });
+window.addEventListener("DOMContentLoaded", (event) => {
+    var mapa=document.getElementById('map');
+    if(mapa=!null){
+        crearMapa();
+        verificarMapa();
+        agregarPunto();
 
-    if (coordenadas.length === 2 && !isNaN(coordenadas[0]) && !isNaN(coordenadas[1])) {
-        var nombre = nombreInput.value.trim();
-        if (nombre !== "") {
-            puntos.push(coordenadas);
-            nombres.push(nombre);
-            puntosInput.value = "";
-            nombreInput.value = "";
-            if (!map) {
-                crearMapa();
-            }
-            console.log(coordenadas)
-            var mark = L.marker([coordenadas[0], coordenadas[1]], { icon: ico2 }).addTo(map).bindPopup("AHHHHHHHHHHH");
-            markers.push(mark);
-            alert("Punto agregado correctamente.");
-        } else {
-            alert("Por favor, ingresa un nombre para el punto.");
-        }
-    } else {
-        alert("Por favor, ingresa las coordenadas correctamente (latitud, longitud).");
     }
+})
+
+function agregarPunto() {
+    puntos.forEach(puntoMapa => {
+        var  coordenadas=puntoMapa.split(',')
+        var mark = L.marker([coordenadas[0], coordenadas[1]], { icon: ico2 }).addTo(map).bindPopup("AHHHHHHHHHHH");
+        markers.push(mark);
+    });
 }
 function crearMapa() {
     if (map == undefined) {
         var x = "-34.89945,-56.13177";
-        map = L.map("map").setView(puntos[0], 13);
+        map = L.map("map").setView(x.split(','), 13);
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
             attribution: "&copy; OpenStreetMap contributors"
         }).addTo(map);
@@ -95,19 +77,5 @@ function crearPuntos(e) {
         .bindPopup("¡Estás aquí!").openPopup();
     circulo = L.circle(e.latlng, radius).addTo(map);
     ubicacion = e.latlng;
-    limpiarRuta();
 }
-
-function limpiarRuta() {
-    // Eliminar rutas existentes
-    ruta.forEach(function (route) {
-        map.removeControl(route);
-    });
-    // Limpiar el arreglo de rutas
-    ruta = [];
-    planRuta = [];
-    r = 0;
-    obtenerCheckboxesActivas();
-}
-
 
