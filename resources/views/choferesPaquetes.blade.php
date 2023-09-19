7897b615440f6c388e300a7d84e70384c870af32
Ignacio Tejera <itejera01@gmail.com>
2023-09-19 15:43:05 -0300

    Vista de choferes paquetes hecha, falta ruta choferes

--- resources/views/choferesPaquetes.blade.php
+++ resources/views/choferesPaquetes.blade.php
@@ -4,11 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/styleAlmacenes.css">
     <title>Document</title>
 </head>
 @include('header')
 <body>
+    <div class="barraDeNavegacion">
 
+        <div class="item"><a style="text-decoration: none; color: black;" href=" /choferes">Volver</a></div>
+
+    </div>
+    <div class="container">
+        <div class="cuerpo">
+            <div id="contenedorTabla">
+                <table>
+                    <thead>
+                        <th>Nombre Paquete</th>
+                        <th>Direccion</th>
+                        <th>Receptor</th>
+                        <th>Entregado</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>10kg de arroz</th>
+                            <th>1023 Perez</th>
+                            <th>Juancho Panza</th>
+                            <th><input type="checkbox" name="entregado" id="entregado"></th>
+                        </tr>
+                        <tr>
+                            <th>10kg de granos de café</th>
+                            <th>1023 Perez</th>
+                            <th>Juancho Panza</th>
+                            <th><input type="checkbox" name="entregado" id="entregado"></th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <br>
 </body>
 @include('footer')
 </html>
