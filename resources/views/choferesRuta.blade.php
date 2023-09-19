4a91b26f943d147f267842dfe65df8008216a705
Ignacio Tejera <itejera01@gmail.com>
2023-09-19 15:46:34 -0300

    Vista de ruta choferes hecha

--- resources/views/choferesRuta.blade.php
+++ resources/views/choferesRuta.blade.php
@@ -4,11 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/styleUbicaciones.css">
+    <script src="https://unpkg.com/leaflet@1.0.2/dist/leaflet.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.2/dist/leaflet.css" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.2.0/dist/leaflet.css" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.css" />
+    <script src="https://unpkg.com/leaflet@1.2.0/dist/leaflet.js"></script>
+    <script src="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js"></script>
+    <script src="js/mapa.js"></script>
     <title>Document</title>
 </head>
 @include('header')
 <body>
-
+    <div class="principalBody">
+        <div>
+            <div id="map">
+            </div>
+        </div>
+        <div style="margin-left: 60px;">
+            <h2>Ubicaciones:</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis nam neque possimus aspernatur </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis nam neque possimus aspernatur </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis nam neque possimus aspernatur </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis nam neque possimus aspernatur </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis nam neque possimus aspernatur </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis nam neque possimus aspernatur </p>
+        </div>
+    </div>
 </body>
 @include('footer')
 </html>
