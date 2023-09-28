7f6542335168ac4188dcc559c522f03205a05a61
Ignacio Tejera <itejera01@gmail.com>
2023-09-28 16:35:43 -0300

    Fixes de carrusel

--- public/js/carrusel.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("carrusel").outerHTML = "";
-carruselDiv.innerHTML += '<img width="125" height="125" src="img/Crecom.png">';
-carruselDiv.innerHTML += '<img width="125" height="125" src="img/Quick Carry.png">';
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -37,7 +37,7 @@
                             </div>
                             <div class="carousel-inner">
                                 <div class="carousel-item active c-item">
-                                    <img id="imagenCarrusel" class="d-block w-80 c-img" src="img/Logo Crecom.png" alt="Crecom">
+                                    <img id="imagenCarrusel" class="d-block w-80 c-img" src="img/Crecom.png" alt="Crecom">
                                 </div>
                                 <div class="carousel-item c-item">
                                     <img id="imagenCarrusel" class="d-block w-80 c-img" src="img/Quick Carry.png" alt="Quick carry">
@@ -83,7 +83,6 @@
         </div>
 
     </div>
-    <script src="js/carrusel.js"></script>
     <script src="resources\js\app.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
